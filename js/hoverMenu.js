fb983160876281e785fc7163d2859d3bba01e9ad
Krišjānis Veinbahs <krisjanis.veinbahs@gmail.com>
2016-08-08 20:18:26 +0300

    Delay on project showcase dropdown header.

--- js/hoverMenu.js
+++ js/hoverMenu.js
@@ -2,19 +2,32 @@ $(document).ready(function(){
     var $menu = $("#header");
     var $hovermenu = $("#hovermenu");
 
+    /* Initial state - hide menu */
     $menu.css({
         'position': "fixed",
         'top': "-300px"
     });
-    $hovermenu.text("Menu");
 
-    $hovermenu.on("mouseenter", function() {
-        $(this).css("top", "-300px");
+    /* Declare menu hide delay timer */
+    var delay = new Timer();
+    var delayTime = 0.5; // seconds
+
+    /* Open menu */
+    $hovermenu.on("mouseover", openMenu);
+    $menu.on("mouseover", openMenu);
+    function openMenu() {
+        delay.pause();
+        $hovermenu.css("top", "-300px");
         $menu.css("top", "0");
-    });
+    }
 
-    $menu.on("mouseleave", function() {
-        $hovermenu.css("top", "0");
-        $(this).css("top", "-300px");
-    });
+    /* Close menu */
+    $menu.on("mouseleave", closeMenu);
+    function closeMenu() {
+        /* Start timer and hide menu after */
+        delay.start(delayTime).on('end', function() {
+            $hovermenu.css("top", "0");
+            $menu.css("top", "-300px");
+        });
+    }
 });
--- readme.md
+++ readme.md
@@ -10,4 +10,5 @@ As minimalistic as a blog can be.
 #### Dependancies
 * [Hakyll](https://jaspervdj.be/hakyll/) for building a static site
 * [Bower](https://bower.io/) for fetching showcase projects
+* [Timer.js](https://husa.github.io/timer.js/) for delay on dropdown menu in project showcases.
 
